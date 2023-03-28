5b8700203a0ea10b22e113b6f34bb19bd005f399
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-28 11:28:37 +0530

    Update Link.java

--- Link.java
+++ Link.java
@@ -10,7 +10,7 @@ public class Link {
         l1.add("morning");
         if(l1.contains(3))
         {
-            System.out.println("The element exist in the list.");
+            System.out.println("The element exists in the list.");
         }
         else
         {
@@ -24,6 +24,6 @@ OUTPUT:
 
 C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=11920:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Ashmi\out\production\Ashmi Link
 
-The element exist in the list.
+The element exists in the list.
 
 Process finished with exit code 0
