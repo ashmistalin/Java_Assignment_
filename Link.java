5fb3361fec0b65ed234e87c64f02a7c23ee20ac2
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-28 11:24:11 +0530

    Update Link.java

--- Link.java
+++ Link.java
@@ -18,3 +18,12 @@ public class Link {
         }
     }
 }
+
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=11920:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Ashmi\out\production\Ashmi Link
+
+The element exist in the list.
+
+Process finished with exit code 0
